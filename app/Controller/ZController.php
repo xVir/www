740712deaaf9aacbd011d8ca27b92305e6070aa8
Danil Skachkov <danil.skachkov@gmail.com>
2012-12-25 15:45:06 +0700

    added Z files

---
+++ app/Controller/ZController.php
@@ -0,0 +1,12 @@
+<?php
+class ZController extends AppController {
+
+	var $uses = array();
+
+	function test() {
+    	//$id = yaz_connect("localhost");
+    	//debug($id);
+	}
+
+}
+?>
---
+++ app/View/Z/test.ctp
@@ -0,0 +1,4 @@
+<?php
+    //phpinfo();
+	 yaz_connect('localhost');
+?>
